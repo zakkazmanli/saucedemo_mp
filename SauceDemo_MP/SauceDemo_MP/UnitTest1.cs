97e68760a9693d3cbb20dd848727200b8f8cd563
Oliver Lawrence-Overy <olawrenceovery@hotmail.com>
2021-02-10 15:22:42 +0000

    Added base project with website class and folder structure

---
+++ SauceDemo_MP/SauceDemo_MP/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SauceDemo_MP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
