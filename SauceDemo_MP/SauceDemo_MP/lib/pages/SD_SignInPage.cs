eb74204fa195fb178a766bccc6a6f9ba7fb2dc83
Oliver Lawrence-Overy <olawrenceovery@hotmail.com>
2021-02-10 15:39:00 +0000

    Added empty page class

---
+++ SauceDemo_MP/SauceDemo_MP/lib/pages/SD_SignInPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SauceDemo_MP.lib.pages
+{
+    public class SD_SignInPage
+    {
+    }
+}
