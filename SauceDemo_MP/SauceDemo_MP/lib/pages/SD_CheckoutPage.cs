7452f68df8696f16a8c287192ea66698349db7b2
Sam Haimes <samhaimes@hotmail.com>
2021-02-11 11:22:46 +0000

    add checkout

--- SauceDemo_MP/SauceDemo_MP/lib/pages/SD_CheckoutPage.cs
+++ SauceDemo_MP/SauceDemo_MP/lib/pages/SD_CheckoutPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using SeleniumPOM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,8 @@ namespace SauceDemo_MP
 {
     public class SD_CheckoutPage
     {
+        private IWebDriver _seleniumDriver;
+
+        private string loginUrl = AppConfigReader.BaseURL;
     }
 }
